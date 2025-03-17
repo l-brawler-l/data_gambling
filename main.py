204463fb5413c648c1b3ce81118cabd1dd9313d3
vakovalik <vasiliy.kovalik@gmail.com>
2025-03-17 03:31:27 +0300

    final

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import creating_json_all_info, install_requirements, ans_model
 import datetime
+
+import ans_model
+import creating_json_all_info
+import install_requirements
 
 if __name__ == '__main__':
     # Обновление json
@@ -11,11 +14,13 @@ if __name__ == '__main__':
     end_city_name = input("Введите название конечного города: ")
     n = int(input("Введите количество промежуточных городов: "))
 
+    rest_time = 0
     mid_points = []
     for i in range(1, n + 1):
         city_name = input(f"Введите название промежуточного города {i}: ")
         time_delta = int(input("Введите количество дней, которые вы планируете в нем провести: "))
         mid_points.append((city_name, datetime.timedelta(days=time_delta)))
+        rest_time += time_delta
 
     date_raw = input("Введите дату отправления в формате 31.12.2025: ").split(".")
 
@@ -26,7 +31,9 @@ if __name__ == '__main__':
     date = datetime.datetime(y, m, d)
 
     max_threads = int(input("Введите максимальное число предложенных маршрутов: "))
-    transport = input("Введите предпочитаемый вид транспорта, например 'самолет' (пустая строчка, если такового нет): ")
+    transport = input("Введите предпочитаемый вид транспорта"
+                      "\nнапример 'самолет, поезд, электричка, автобус, морской транспорт' "
+                      "\n(пустая строчка, если такового нет): ")
 
     match transport:
         case 'самолет':
@@ -44,4 +51,4 @@ if __name__ == '__main__':
         case _:
             transport = ''
 
-    ans_model.answer(start_city_name, end_city_name, date, mid_points, max_threads, transport)
+    ans_model.answer(start_city_name, end_city_name, date, mid_points, max_threads, transport, rest_time)
